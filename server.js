8cca36fcf503a33484ca10c3b78bab201b7ca296
NamrahAkram2655 <namrahakram102@gmail.com>
2025-04-07 00:10:21 +0500

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ const port = process.env.PORT || 5000;
 connectdb();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://green-heaven-seven.vercel.app", // Your frontend URL
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.urlencoded({ extended: true })); //html form submission
 
 // API Endpoints
